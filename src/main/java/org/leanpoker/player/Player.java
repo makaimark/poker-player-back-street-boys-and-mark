4b5737cac4bed25bd697dd665e398ffafa29063b
Áron Kékes <kekes.aron@gmail.com>
2017-01-12 13:51:37 +0100

    m

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -39,7 +39,7 @@ public class Player {
 
         JsonArray community_cards = jObject.get( "community_cards").getAsJsonArray();
 
-        System.err.println("Community_cards: " + community_cards.get(0) + community_cards.get(1) + community_cards.get(2));
+//        System.err.println("Community_cards: " + community_cards.get(0) + community_cards.get(1) + community_cards.get(2));
         List<String> highcards = Arrays.asList("J", "Q", "K", "A");
         List<String> mediumcards = Arrays.asList("8", "9", "10");
 
