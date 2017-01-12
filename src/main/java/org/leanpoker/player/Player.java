ff7e0cfef707e62653ddc70b7e9ecdf3127f59b5
Áron Kékes <kekes.aron@gmail.com>
2017-01-12 14:38:21 +0100

    m

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -52,18 +52,23 @@ public class Player {
         if (community_cards.size() == 0) {
             //If we don't have flop
             if (highcards.contains(rank0) && highcards.contains(rank1)) {
+                System.err.println("Without flop, high cards");
                 return jObject.get("current_buy_in").getAsInt() + jObject.get("minimum_raise").getAsInt();
             } else if ((highcards.contains(rank0) && mediumcards.contains(rank1)) || (highcards.contains(rank1) && mediumcards.contains(rank0))) {
+                System.err.println("Without flop, high and medium card");
                 return jObject.get("current_buy_in").getAsInt() - details.get("bet").getAsInt();
             } else if (rank0.equals(rank1)) {
+                System.err.println("Without flop, hand pair");
                 return jObject.get("current_buy_in").getAsInt() + jObject.get("minimum_raise").getAsInt();
             } else {
                 return 0;
             }
         } else {
             if (commCards.contains(rank0) || commCards.contains(rank1)) {
+                System.err.println("flop-turn-river - pair");
                 return jObject.get("current_buy_in").getAsInt() - details.get("bet").getAsInt();
             } else {
+                System.err.println("flop-turn-river without pair");
                 return 0;
             }
         }
