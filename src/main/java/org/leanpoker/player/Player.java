5d3555d4c7c51276421a45fb8e22238c6dfc165e
Áron Kékes <kekes.aron@gmail.com>
2017-01-12 12:16:10 +0100

    m

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,11 +13,11 @@ public class Player {
     public static int betRequest(JsonElement request) {
 
         JsonObject jObject = request.getAsJsonObject();
-       // JsonObject inAction =  jObject.getAsJsonObject("in_action");
+        String inAction =  jObject.get("in_action").getAsString();
         JsonArray players = jObject.getAsJsonArray("players");
         System.err.println("\n_____________________________________________________________________");
         System.err.println("LOG: " + players);
-        //System.err.println("in action: " + inAction);
+        System.err.println("in action: " + Integer.parseInt(inAction));
         System.err.println("_____________________________________________________________________\n");
 
 //        JsonObject json = request.getAsJsonObject();
